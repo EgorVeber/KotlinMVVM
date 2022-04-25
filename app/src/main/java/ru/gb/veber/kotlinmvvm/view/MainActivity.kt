a2262b382c286390f137c45b96a679a2c0fbcd61
JamesOhara <egor.veber.02.07.95@gmail.com>
2022-04-25 17:54:43 +0300

    Start

--- app/src/main/java/ru/gb/veber/kotlinmvvm/view/MainActivity.kt
+++ app/src/main/java/ru/gb/veber/kotlinmvvm/view/MainActivity.kt
@@ -20,7 +20,6 @@ class MainActivity : AppCompatActivity() {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.fragment_container, CitysFragment()).commit()
         }
-
     }
 
     override fun onBackPressed() {
--- app/src/main/java/ru/gb/veber/kotlinmvvm/view/fragment/DetailsFragment.kt
+++ app/src/main/java/ru/gb/veber/kotlinmvvm/view/fragment/DetailsFragment.kt
@@ -1,8 +1,10 @@
 package ru.gb.veber.kotlinmvvm.view.fragment
 
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.*
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -12,7 +14,9 @@ import ru.gb.veber.kotlinmvvm.R
 import ru.gb.veber.kotlinmvvm.databinding.FragmentDetailsBinding
 import ru.gb.veber.kotlinmvvm.model.Hours
 import ru.gb.veber.kotlinmvvm.model.Weather
+import ru.gb.veber.kotlinmvvm.model.WeatherDTO
 import ru.gb.veber.kotlinmvvm.model.formatDate
+import ru.gb.veber.kotlinmvvm.view.WeatherLoader
 import ru.gb.veber.kotlinmvvm.view.adapter.AdapterHour
 import ru.gb.veber.kotlinmvvm.view.adapter.AdapterWeek
 import ru.gb.veber.kotlinmvvm.view_model.AppState
@@ -26,6 +30,7 @@ class DetailsFragment : Fragment() {
     private val binding get() = _binding!!
     private val adapterHour = AdapterHour()
     private val adapterWeek = AdapterWeek()
+    private lateinit var weatherBundle: Weather
 
     companion object {
         const val KEY_WEATHER = "KEY_WEATHER"
@@ -41,6 +46,7 @@ class DetailsFragment : Fragment() {
         return binding.root
     }
 
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setHasOptionsMenu(true)
@@ -55,19 +61,33 @@ class DetailsFragment : Fragment() {
                 LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
         }
 
-        arguments?.getParcelable<Weather>(KEY_WEATHER)?.let { weather ->
-            binding.apply {
-                cityName.text = weather.city.cityName
-                dataText.text = weather.time.formatDate()
-                weatherIcon.background = resources.getDrawable(R.drawable.sun264)
-                weatherText.text = weather.temperature.toString()
-                conditionText.text = weather.condition
-                feelsLikeText.text =
-                    resources.getString(R.string.feelsLike) + " " + weather.feelsLike.toString()
-                adapterHour.setWeather(weather.forecastList[0].hours)
-                adapterWeek.setWeather(weather.forecastList)
+        weatherBundle = arguments?.getParcelable<Weather>(KEY_WEATHER) ?: Weather()
+        Log.d("TAG", "$weatherBundle")
+        val loader = WeatherLoader(onLoadListener, weatherBundle.city.lat, weatherBundle.city.lon)
+        loader.loadWeather()
+    }
+
+    private val onLoadListener: WeatherLoader.WeatherLoaderListener =
+        object : WeatherLoader.WeatherLoaderListener {
+            override fun onLoaded(weatherDTO: WeatherDTO) {
+                displayWeather(weatherDTO)
+            }
+
+            override fun onFailed(throwable: Throwable) {
             }
         }
+
+    private fun displayWeather(weatherDTO: WeatherDTO) {
+        with(binding)
+        {
+            weatherDTO.fact.apply {
+                cityName.text = weatherBundle.city.cityName
+                feelsLikeText.text = feels_like.toString()
+                conditionText.text = condition
+                weatherText.text = temp.toString()
+            }
+        }
+        Log.d("TAG", "displayWeather() called with: weatherDTO = $weatherDTO")
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
