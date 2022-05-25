e213fffacb53b3a5ba1c325a39c610bb3279d408
JamesOhara <egor.veber.02.07.95@gmail.com>
2022-05-25 18:34:29 +0300

    add call contacts

--- app/src/main/java/ru/gb/veber/kotlinmvvm/view/fragment/ContentProviderFragment.kt
+++ app/src/main/java/ru/gb/veber/kotlinmvvm/view/fragment/ContentProviderFragment.kt
@@ -3,29 +3,33 @@ package ru.gb.veber.kotlinmvvm.view.fragment
 import android.Manifest
 import android.annotation.SuppressLint
 import android.content.ContentResolver
-import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.database.Cursor
 import android.net.Uri
 import android.os.Bundle
+import android.os.Handler
+import android.os.HandlerThread
 import android.provider.ContactsContract
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.widget.AppCompatTextView
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import ru.gb.veber.kotlinmvvm.R
 import ru.gb.veber.kotlinmvvm.databinding.FragmentContentProviderBinding
+import ru.gb.veber.kotlinmvvm.view.adapter.AdapterContacts
+import ru.gb.veber.kotlinmvvm.view.adapter.CitysFragmentAdapter
+import ru.gb.veber.kotlinmvvm.view.adapter.OnContactsClickListener
 
 
 const val REQUEST_CODE = 44
 
-class ContentProviderFragment : Fragment() {
+class ContentProviderFragment : Fragment(), OnContactsClickListener {
     private var _binding: FragmentContentProviderBinding? = null
     private val binding get() = _binding!!
+
+    private val adapter = AdapterContacts()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -37,6 +41,9 @@ class ContentProviderFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.listContacts.adapter = adapter
+        adapter.setOnContactsClickListener(this)
         checkPermission()
     }
 
@@ -51,20 +58,15 @@ class ContentProviderFragment : Fragment() {
             ContentProviderFragment()
     }
 
-    private fun addView(context: Context, textToShow: String) {
-        binding.containerForContacts.addView(AppCompatTextView(context).apply {
-            text = textToShow
-            textSize = resources.getDimension(R.dimen.main_container_text_size)
-        })
-    }
-
     private fun checkPermission() {
         context?.let {
             when {
                 ContextCompat.checkSelfPermission(it, Manifest.permission.READ_CONTACTS) ==
                         PackageManager.PERMISSION_GRANTED -> {
                     Log.d("TAG", "getContacts()")
+
                     getContacts()
+
                 }
                 shouldShowRequestPermissionRationale(Manifest.permission.READ_CONTACTS) -> {
                     Log.d("TAG", "shouldShowRequestPermissionRationale")
@@ -97,7 +99,9 @@ class ContentProviderFragment : Fragment() {
         when (requestCode) {
             REQUEST_CODE -> {
                 if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
                     getContacts()
+
                 } else {
                     context?.let {
                         androidx.appcompat.app.AlertDialog.Builder(it)
@@ -112,45 +116,58 @@ class ContentProviderFragment : Fragment() {
         }
     }
 
-    @SuppressLint("Range")
     private fun getContacts() {
-        context?.let {
-            val contentResolver: ContentResolver = it.contentResolver
 
-            val cursorWithContacts: Cursor? = contentResolver.query(
-                ContactsContract.Contacts.CONTENT_URI,
-                null,
-                null,
-                null,
-                ContactsContract.Contacts.DISPLAY_NAME + " ASC"
-            )
-            val cursorWithContacts2: Cursor? = contentResolver.query(
-                ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
-                null,
-                null,
-                null,
-                ContactsContract.Contacts.DISPLAY_NAME_PRIMARY
-            )
+        var listContacts: MutableList<MyContacts> = mutableListOf()
 
-            cursorWithContacts?.let { cursor ->
-                for (i in 0..cursor.count) {
-                    val pos = cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME)
-                    val pos2 =
-                        cursorWithContacts2?.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER)
 
-                    if (cursor.moveToPosition(i) && cursorWithContacts2?.moveToPosition(i) == true) {
-                        val name = cursor.getString(pos)
-                        val name2 = cursorWithContacts2?.getString(pos2!!)
-                        addView(it, "$name  $name2")
+        var handlerThread = Handler()
+        Thread {
+            context?.let {
+                val contentResolver: ContentResolver = it.contentResolver
+
+
+                val cursorWithContacts: Cursor? = contentResolver.query(
+                    ContactsContract.Contacts.CONTENT_URI,
+                    null,
+                    null,
+                    null,
+                    ContactsContract.Contacts.DISPLAY_NAME + " ASC"
+                )
+                val cursorWithContacts2: Cursor? = contentResolver.query(
+                    ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                    null,
+                    null,
+                    null,
+                    ContactsContract.Contacts.DISPLAY_NAME_PRIMARY
+                )
+
+                cursorWithContacts?.let { cursor ->
+                    for (i in 0..cursor.count) {
+                        val pos = cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME)
+                        val pos2 =
+                            cursorWithContacts2?.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER)
+
+                        if (cursor.moveToPosition(i) && cursorWithContacts2?.moveToPosition(i) == true) {
+                            val name = cursor.getString(pos)
+                            val name2 = cursorWithContacts2?.getString(pos2!!)
+                            listContacts.add(MyContacts(name, name2))
+                        }
                     }
                 }
+                cursorWithContacts?.close()
+                cursorWithContacts2?.close()
             }
-            cursorWithContacts?.close()
-            cursorWithContacts2?.close()
-        }
+            handlerThread.post {
+                adapter.setWeather(listContacts)
+            }
+        }.start()
+    }
 
-        val phone = "+34666777888"
-        val intent = Intent(Intent.ACTION_DIAL, Uri.fromParts("tel", phone, null))
+    override fun onCityClick(contacts: MyContacts) {
+        val intent = Intent(Intent.ACTION_DIAL, Uri.fromParts("tel", contacts.phone, null))
         startActivity(intent)
     }
 }
+
+data class MyContacts(var name: String, var phone: String)
